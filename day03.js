cc611322b3a9be8c218761d7e890f6ac23fed751
sgm4247 <anjuhyeon@anjuhyeon-ui-MacBookAir.local>
2023-06-19 23:42:25 +0900

    문제풀이 3번 재풀이

--- day03.js
+++ day03.js
@@ -1,5 +1,5 @@
 /*3. 배열 다루기
-당신이 구현하고하자 하는 함수는
+당신이 구현하고하자 하는 함수는  
 배열과 정수를 파라미터로 전달 받습니다. 함수 이름은 정해지지 않았으니 마음대로 하셔도 괜찮습니다. 
 전달받은 배열은 정확히 각 요소마다 값을 10을 더 합니다. 이후 값이 10씩 더해진 배열에서, 
 나머지 파라미터로 전달받은 정수가 있는지 확인하여
@@ -24,3 +24,38 @@ console.log (a)
 // console.log(name === "juhyeon" ? "yes" : "no");
 
 //이게뭔데엥..
+
+// 6/18 19일 째
+/*
+function map(arr, callback) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(callback(arr[i], i, arr));
+  }
+  return result;
+}
+
+const arr = [1, 2, 3, 4, 5];
+const a = map(arr, (item) => {
+  return item + 10;
+});
+console.log(a);
+*/
+
+function random(arr, test) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(arr[i] + 10);
+    // result.shift();
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (test === test) return result.slice(1);
+    // 요기부터 내일 필터로 다시 해보자!
+
+    if (result[i] === test) return " 값이 있음";
+  }
+  return " 값이 없음";
+}
+const arr = [1, 2, 3, 4, 5];
+const a = random(arr, 11);
+console.log(a);
