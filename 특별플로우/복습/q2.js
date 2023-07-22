91fe29f07850554f8b387b613062267d0a695ac1
sgm4247 <sgm4247@naver.com>
2023-07-22 11:35:24 +0900

    복습/q2

--- 특별플로우/복습/q2.js
+++ 특별플로우/복습/q2.js
@@ -29,15 +29,15 @@ const users = [
 CRUD 구현하기
 배열의 고차함수
 
-문제1. 유저 추가하기
+문제1. 유저 추가하기 쉬퓌
   내가 추가하고자 하는 유저를 추가해야합니다
   단 id는 고유 번호로 반드시 순서대로일 필요는 없지만, 어떠한 경우에도 겹쳐서 안됩니다
 
-문제2. 유저 삭제하기
+문제2. 유저 삭제하기 스플
   내가 원하는 유저를 삭제할 수 있어야합니다.
   단, 동일한 유저의 이름이 있더라도 정확히 원하는 유저여야합니다.
 
-문제3. 유저 수정하기
+문제3. 유저 수정하기 
   내가 수정하고자 하는 유저의 개인정보를 수정해야합니다.
 
 문제4. 유저 조회하기
@@ -47,5 +47,33 @@ CRUD 구현하기
 문제5. 조회한 유저를 height별 오름 차순으로 정렬하여 조회하기
 */
 //문제1. 유저 추가하기
-const filter = users.filter((add) => add.id === users[add]);
-user.push();
+const addPost = {
+  id: 5,
+  name: "추가명",
+  age: 40,
+  height: 155,
+};
+users.push(addPost);
+console.log(users);
+// const userspush = users.push((add) => add.id === users[add]);
+
+//문제2. 유저 삭제하기
+
+const delPost = users.splice(4, 1);
+console.log(users);
+
+//문제3. 유저 수정하기 실행하려면 문제 2번을 지우고 실행
+const findPost = users.find((post) => post.id == 5);
+addPost.age = 50;
+console.log(users);
+
+//문제4. 유저 조회하기
+// const fiiterPost = users.filter((post) => post.id !== 0);
+// console.log(fiiterPost);
+console.log(users);
+
+//문제5. 조회한 유저를 height별 오름 차순으로 정렬하여 조회하기
+const sortusers = users.filter((post) => post.height);
+sortusers.sort((a, b) => a.height - b.height);
+// sortusers.height = sortusers.sort((a, b) => a - b);
+console.log(sortusers);
